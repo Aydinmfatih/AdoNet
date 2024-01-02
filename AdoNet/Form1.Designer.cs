89d95d299bf5d7d4361d7f412ac6c012740a4e7a
aydinmfatih <aydinmfatih@gmail.com>
2024-01-02 20:18:54 +0300

    Add project files.

---
+++ AdoNet/Form1.Designer.cs
@@ -0,0 +1,227 @@
+﻿
+namespace AdoNet
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnList = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnUpdate = new System.Windows.Forms.Button();
+            this.btnDelete = new System.Windows.Forms.Button();
+            this.btnGet = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.ProjectTitle = new System.Windows.Forms.Label();
+            this.txtTitle = new System.Windows.Forms.TextBox();
+            this.Category = new System.Windows.Forms.Label();
+            this.txtProccess = new System.Windows.Forms.TextBox();
+            this.Proccess = new System.Windows.Forms.Label();
+            this.txtPrice = new System.Windows.Forms.TextBox();
+            this.Price = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnList
+            // 
+            this.btnList.Location = new System.Drawing.Point(12, 32);
+            this.btnList.Name = "btnList";
+            this.btnList.Size = new System.Drawing.Size(88, 36);
+            this.btnList.TabIndex = 0;
+            this.btnList.Text = "Listele";
+            this.btnList.UseVisualStyleBackColor = true;
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(12, 74);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(88, 34);
+            this.btnAdd.TabIndex = 1;
+            this.btnAdd.Text = "Ekle";
+            this.btnAdd.UseVisualStyleBackColor = true;
+            // 
+            // btnUpdate
+            // 
+            this.btnUpdate.Location = new System.Drawing.Point(12, 161);
+            this.btnUpdate.Name = "btnUpdate";
+            this.btnUpdate.Size = new System.Drawing.Size(88, 34);
+            this.btnUpdate.TabIndex = 3;
+            this.btnUpdate.Text = "Güncelle";
+            this.btnUpdate.UseVisualStyleBackColor = true;
+            // 
+            // btnDelete
+            // 
+            this.btnDelete.Location = new System.Drawing.Point(12, 121);
+            this.btnDelete.Name = "btnDelete";
+            this.btnDelete.Size = new System.Drawing.Size(88, 34);
+            this.btnDelete.TabIndex = 4;
+            this.btnDelete.Text = "Sil";
+            this.btnDelete.UseVisualStyleBackColor = true;
+            // 
+            // btnGet
+            // 
+            this.btnGet.Location = new System.Drawing.Point(12, 201);
+            this.btnGet.Name = "btnGet";
+            this.btnGet.Size = new System.Drawing.Size(88, 34);
+            this.btnGet.TabIndex = 5;
+            this.btnGet.Text = "Getir";
+            this.btnGet.UseVisualStyleBackColor = true;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(12, 241);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(776, 197);
+            this.dataGridView1.TabIndex = 6;
+            // 
+            // ProjectTitle
+            // 
+            this.ProjectTitle.AutoSize = true;
+            this.ProjectTitle.Location = new System.Drawing.Point(205, 32);
+            this.ProjectTitle.Name = "ProjectTitle";
+            this.ProjectTitle.Size = new System.Drawing.Size(94, 17);
+            this.ProjectTitle.TabIndex = 7;
+            this.ProjectTitle.Text = "Proje Başlığı :";
+            this.ProjectTitle.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // txtTitle
+            // 
+            this.txtTitle.Location = new System.Drawing.Point(311, 32);
+            this.txtTitle.Name = "txtTitle";
+            this.txtTitle.Size = new System.Drawing.Size(100, 22);
+            this.txtTitle.TabIndex = 8;
+            // 
+            // Category
+            // 
+            this.Category.AutoSize = true;
+            this.Category.Location = new System.Drawing.Point(205, 60);
+            this.Category.Name = "Category";
+            this.Category.Size = new System.Drawing.Size(69, 17);
+            this.Category.TabIndex = 9;
+            this.Category.Text = "Kategori :";
+            // 
+            // txtProccess
+            // 
+            this.txtProccess.Location = new System.Drawing.Point(311, 88);
+            this.txtProccess.Name = "txtProccess";
+            this.txtProccess.Size = new System.Drawing.Size(100, 22);
+            this.txtProccess.TabIndex = 12;
+            // 
+            // Proccess
+            // 
+            this.Proccess.AutoSize = true;
+            this.Proccess.Location = new System.Drawing.Point(205, 88);
+            this.Proccess.Name = "Proccess";
+            this.Proccess.Size = new System.Drawing.Size(112, 17);
+            this.Proccess.TabIndex = 11;
+            this.Proccess.Text = "Tamamlanma O.";
+            // 
+            // txtPrice
+            // 
+            this.txtPrice.Location = new System.Drawing.Point(311, 121);
+            this.txtPrice.Name = "txtPrice";
+            this.txtPrice.Size = new System.Drawing.Size(100, 22);
+            this.txtPrice.TabIndex = 14;
+            // 
+            // Price
+            // 
+            this.Price.AutoSize = true;
+            this.Price.Location = new System.Drawing.Point(205, 121);
+            this.Price.Name = "Price";
+            this.Price.Size = new System.Drawing.Size(46, 17);
+            this.Price.TabIndex = 13;
+            this.Price.Text = "Fiyat :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(507, 31);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(109, 17);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Proje Detayları :";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(311, 60);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 24);
+            this.comboBox1.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtPrice);
+            this.Controls.Add(this.Price);
+            this.Controls.Add(this.txtProccess);
+            this.Controls.Add(this.Proccess);
+            this.Controls.Add(this.Category);
+            this.Controls.Add(this.txtTitle);
+            this.Controls.Add(this.ProjectTitle);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.btnGet);
+            this.Controls.Add(this.btnDelete);
+            this.Controls.Add(this.btnUpdate);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnList);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnList;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnUpdate;
+        private System.Windows.Forms.Button btnDelete;
+        private System.Windows.Forms.Button btnGet;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label ProjectTitle;
+        private System.Windows.Forms.TextBox txtTitle;
+        private System.Windows.Forms.Label Category;
+        private System.Windows.Forms.TextBox txtProccess;
+        private System.Windows.Forms.Label Proccess;
+        private System.Windows.Forms.TextBox txtPrice;
+        private System.Windows.Forms.Label Price;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox1;
+    }
+}
+
